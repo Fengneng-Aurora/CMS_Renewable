90c1a8fa7e64b1c4816ba7f2a0ba864de437aa51
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 20:42:13 +0800

    Initial commit

---
+++ CMS Renewable/README.txt
@@ -0,0 +1,2 @@
+A mod that uses Create to regenerate many resources
+- MOD for CMS server
---
+++ CMS Renewable/src/main/java/com/cms/cmsrenewable/groups/ModGroup.java
@@ -0,0 +1,20 @@
+package com.cms.cmsrenewable.groups;
+
+import com.cms.cmsrenewable.CMSRenewable;
+import com.cms.cmsrenewable.index.CMSRItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModGroup extends CreativeModeTab {
+    public static ModGroup MAIN;
+
+    public ModGroup(String name) {
+        super(CMSRenewable.MODID + ":" + name);
+        MAIN = this;
+    }
+
+    @Override
+    public ItemStack makeIcon() {
+        return new ItemStack(CMSRItems.IRON_ELYTRA_FRAME.get());
+    }
+}
