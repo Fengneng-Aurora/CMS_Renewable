5fa332097ea4b29c5e004db104ec54b6788282c8
BetaFoprhoton <96339528+BetaFoprhoton@users.noreply.github.com>
2024-07-16 16:04:02 +0800

    5.1

--- src/main/java/com/cms/cmsrenewable/groups/ModGroup.java
+++ src/main/java/com/cms/cmsrenewable/groups/ModGroup.java
@@ -2,8 +2,12 @@ package com.cms.cmsrenewable.groups;
 
 import com.cms.cmsrenewable.CMSRenewable;
 import com.cms.cmsrenewable.index.CMSRItems;
+import com.simibubi.create.repack.registrate.util.entry.FluidEntry;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.PowderSnowBlock;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.Tags;
 
 public class ModGroup extends CreativeModeTab {
     public static ModGroup MAIN;
@@ -11,6 +15,8 @@ public class ModGroup extends CreativeModeTab {
     public ModGroup(String name) {
         super(CMSRenewable.MODID + ":" + name);
         MAIN = this;
+        PowderSnowBlock;
+        Fluids
     }
 
     @Override
